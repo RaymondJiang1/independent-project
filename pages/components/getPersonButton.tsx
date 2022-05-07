61ebfa396be79020f4a5372c4d20bcde87d1ca64
RaymondJiang1 <raymondjiang@casady.org>
2022-05-07 04:28:57 +0000

    works

--- pages/components/getPersonButton.tsx
+++ pages/components/getPersonButton.tsx
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 import { getPerson } from '../functions/functions';
 
 export class GetPersonButton extends Component {
-	onClick() {
-		getPerson();
+	constructor(props = {}) {
+		super(props);
+		this.state = {
+			image: 'https://elipticon.com/wp/wp-content/uploads/2019/06/click-down-here.png',
+		};
 	}
 
 	render() {
-		return <button onClick={this.onClick}></button>;
+		return (
+			<div className='person'>
+				<button
+					onClick={async () =>
+						this.setState({
+							image: await getPerson(),
+						})
+					}>
+					Click me!
+				</button>
+				<img
+					src={this.state.image}
+					alt='some pereson'
+					width={505}
+					height={505}
+				/>
+			</div>
+		);
 	}
 }
